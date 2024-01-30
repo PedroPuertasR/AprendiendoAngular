33eba24d03105ad2e3b9e0edc1bc84dbd9e398ed
Pedro Puertas Rodríguez <pedropuertasr@gmail.com>
2024-01-30 18:37:37 +0100

    Implementando Spring Security

---
+++ apirest.ejemplo/src/main/java/com/pedropuertas/apirest/ejemplo/model/Role.java
@@ -0,0 +1,32 @@
+package com.pedropuertas.apirest.ejemplo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "roles")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class Role implements Serializable {
+
+    @Serial
+    @Getter(AccessLevel.NONE)
+    @Setter(AccessLevel.NONE)
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, length = 20)
+    private String nombre;
+
+
+
+}
---
+++ apirest.ejemplo/src/main/java/com/pedropuertas/apirest/ejemplo/model/Usuario.java
@@ -0,0 +1,36 @@
+package com.pedropuertas.apirest.ejemplo.model;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario implements Serializable {
+
+    @Serial
+    @Getter(AccessLevel.NONE)
+    @Setter(AccessLevel.NONE)
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, length = 20)
+    private String username;
+
+    @Column(length = 60)
+    private String password;
+
+    private Boolean enabled;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<Role> roles;
+
+}
